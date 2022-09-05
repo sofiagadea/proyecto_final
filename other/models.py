a454bb2d8119e5183ff9e85721e63e8da5bc6164
sofiagadea <slgadea@miuandes.cl>
2022-09-05 16:17:19 -0300

    tercero

--- other/models.py
+++ other/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    statement = models.TextField()
+
+    def __str__(self):
+        return self
